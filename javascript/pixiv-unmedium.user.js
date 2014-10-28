7ac5f3f3381f408d23e77e4b2a18cb5bab41968b
Shota Fukumori (sora_h) <her@sorah.jp>
2014-10-29 01:25:55 +0900

    pixiv-unmedium: Fix manga view

--- javascript/pixiv-unmedium.user.js
+++ javascript/pixiv-unmedium.user.js
@@ -8,6 +8,20 @@
 // @run-at document-end
 // ==/UserScript==
 
+var unmedium = function (src) {
+  if (src.match(/img-master/)) {
+    var m = src.match(/http:\/\/(.+?)\.pixiv.net/);
+    var host = "i3.pixiv.net";
+    if (m) host = m[1] + ".pixiv.net";
+
+    src = "http://" + host + "/img-original/img/" + src.match(/\/img\/(.+)$/)[1];
+    src = src.replace(/_master\d+/, '');
+  } else {
+    src = src.replace(/_m/, '');
+  }
+  return src;
+}
+
 var link = document.querySelector(".works_display a");
 if (link && !link.href.match(/mode=manga/)) {
   var img = document.querySelector(".works_display img");
@@ -17,12 +31,7 @@ if (link && !link.href.match(/mode=manga/)) {
     img.setAttribute('width', img.width);
     img.onload = null;
     var originalSrc = img.src;
-    if (img.src.match(/img-master/)) {
-      img.src = "http://i3.pixiv.net/img-original/img/" + img.src.match(/\/img\/(.+)$/)[1];
-      img.src = img.src.replace(/_master\d+/, '');
-    } else {
-      img.src = img.src.replace(/_m/, '');
-    }
+    src = unmedium(src);
     document.querySelector(".works_display").innerHTML += "<p>" + originalSrc + "<br>" + img.src + "</p>";
   };
 
@@ -40,7 +49,7 @@ if (location.search.match(/mode=manga/)) {
     var container = containers[i];
     var img = container.querySelector('img');
 
-    img.src = img.attributes['data-src'].value.replace(/_p/, '_big_p');
+    img.src = unmedium(img.attributes['data-src'].value);
     container.innerHTML += "<p>" + img.src + "</p>";
   }
 }
