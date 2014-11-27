b469fa3bbd9e0f9dd3046655d870a610839748d9
Shota Fukumori (sora_h) <her@sorah.jp>
2014-11-28 01:57:22 +0900

    Add twitter auto update

--- ruby/djay_tw.rb
+++ ruby/djay_tw.rb
@@ -1,3 +1,12 @@
+require 'twitter'
+prefix = ARGV[0] || ''
+prefix << ' ' unless prefix.empty?
+tw = Twitter::REST::Client.new(
+  consumer_key: ENV['TWITTER_CONSUMER_KEY'],
+  consumer_secret: ENV['TWITTER_CONSUMER_SECRET'],
+  access_token: ENV['TWITTER_ACCESS_TOKEN'],
+  access_token_secret: ENV['TWITTER_ACCESS_TOKEN_SECRET'],
+)
 path = File.expand_path('~/Music/djay/NowPlaying.txt')
 
 prev = nil
@@ -6,14 +15,15 @@ t = nil
 loop do
   now = File.read(path).each_line.map { |_| _.chomp.split(/: /,2) }.to_h
   if now != prev
-    text = "#kosendj Now Playing: #{now['Title']} (#{now['Artist']})"
+    text = "#{prefix}Now Playing: #{now['Title']} (#{now['Artist']})"
     if text.size > 135
-      text = "#kosendj Now Playing: #{now['Title']}"
+      text = "#{prefix}Now Playing: #{now['Title']}"
     end
     prev = now
     pret = t
     t = Time.now
     puts "#{t.to_s}: #{text} (#{t-(pret || Time.now)})"
+    tw.update(text)
   end
   sleep 1
 end
